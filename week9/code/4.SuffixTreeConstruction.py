09f69ab285b4294377c5baab43847fd9c4f406e1
ashis <alorchhota@gmail.com>
2014-04-22 10:06:31 +0900

    week-9 problem-3&4 solved.

--- week9/code/4.SuffixTreeConstruction.py
+++ week9/code/4.SuffixTreeConstruction.py
@@ -8,9 +8,9 @@ import networkx as nx
 # settings
 #curDir = 'E:/GitHub/bioinformatics-algorithms-1/week9'
 curDir = 'D:/Copy/Coursera/Bioinformatics Algorithms (part-I)/MyPrograms/week9'
-inputFile = './data/3.LongestRepeat-2.txt'
-inputFile = 'C:/Users/Ashis/Downloads/dataset_295_8.txt'
-outputFile = './results/3.LongestRepeat.txt'
+inputFile = './data/4.SuffixTreeConstruction-2.txt'
+inputFile = 'C:/Users/Ashis/Downloads/dataset_296_4.txt'
+outputFile = './results/4.SuffixTreeConstruction.txt'
 
 # set current directory
 os.chdir(curDir)
@@ -19,7 +19,7 @@ os.chdir(curDir)
 with open(inputFile) as f:
     inputs = f.readlines()
 
-genome = inputs[0].strip() + '$'
+genome = inputs[0].strip()
 #print(genome)
 #print(patterns)
 
@@ -97,30 +97,12 @@ for idx in range(genomeLen):
             break
 
 
-## find the longest repeat
-# traverse the tree in bfs manner and update string length at nodes
-edges = nx.dfs_edges(g)
-lengthAttr = 'length'
-g.node[1][lengthAttr] = 0
-for e in edges:
-    if genome[g.edge[e[0]][e[1]]['labelIdx'][1]-1] == '$':
-        g.node[e[1]][lengthAttr] = 0
-    else:
-        g.node[e[1]][lengthAttr] = g.node[e[0]][lengthAttr] + g.edge[e[0]][e[1]]['labelIdx'][1] - g.edge[e[0]][e[1]]['labelIdx'][0]
-
-# find the max length node
-lengths = [(n, g.node[n][lengthAttr]) for n in g.nodes()]
-longestNode = max(lengths, key=lambda x:x[1])
-print(longestNode)
-
-# the path from the root to the longestNode is the longest repeat
-path = nx.shortest_path(g, 1, longestNode[0])
-edgeLabels = [genome[g.edge[path[i-1]][path[i]]['labelIdx'][0]:g.edge[path[i-1]][path[i]]['labelIdx'][1]] for i in range(1,len(path))]
-longestRepeat = "".join(edgeLabels)
-print(longestRepeat)
+edgeLabels = [genome[e[2]['labelIdx'][0]:e[2]['labelIdx'][1]] for e in g.edges_iter(data=True)]
+#print(edgeLabels)
+    
 
 ## output
 with open(outputFile, "w") as f:
-    f.writelines(longestRepeat)
+    f.writelines('\n'.join(edgeLabels))
 
 print('done.')

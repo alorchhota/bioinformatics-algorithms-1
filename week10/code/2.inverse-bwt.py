8814957d75b46f5575ea942743134ee3fece79aa
ashis <alorchhota@gmail.com>
2014-04-24 13:46:21 +0900

    week-10 problem-2 solved.

--- week10/code/2.inverse-bwt.py
+++ week10/code/2.inverse-bwt.py
@@ -7,10 +7,10 @@ import networkx as nx
 import itertools
 
 # settings
-#curDir = 'E:/GitHub/bioinformatics-algorithms-1/week10'
-curDir = 'D:/GitHub/bioinformatics-algorithms-1/week10'
-inputFile = './data/2.inverse-bwt-1.txt'
-#inputFile = 'C:/Users/Ashis/Downloads/dataset_297_4.txt'
+curDir = 'E:/GitHub/bioinformatics-algorithms-1/week10'
+#curDir = 'D:/GitHub/bioinformatics-algorithms-1/week10'
+inputFile = './data/2.inverse-bwt-2.txt'
+#inputFile = 'C:/Users/Ashis/Downloads/dataset_299_10.txt'
 outputFile = './results/2.inverse-bwt.txt'
 
 # set current directory
@@ -24,13 +24,44 @@ genome = inputs[0].strip()
 
 def ibwt(bwtgenome):
     """ Given BWT(genome), get genome. """
-    firstRow = sorted([nt for nt in bwtgenome])
-    print(''.join(firstRow))
-    while 
+
+    genomeLen = len(bwtgenome)
     
+    # construct the first col
+    firstCol = sorted([nt for nt in bwtgenome])
+
+    # find unique letters in the genome
+    alphabets = sorted(list(set(firstCol)))
+
+    # find the index of first occurence of each letter in the first col
+    start, firstColIndex = 0, {}
+    for letter in alphabets:
+        firstColIndex[letter] = start = firstCol.index(letter, start)
+        
+    # count the occurence in the last column
+    freq = {letter:0 for letter in alphabets}
+    lastColCount = [0]*genomeLen
+    for i in range(genomeLen):
+        letter = bwtgenome[i]
+        lastColCount[i] = freq[letter]
+        freq[letter] += 1
+        
+    # iterate using first-last property
+    fidx = 0
+    genome = ['$']
+    while True:
+        letter = bwtgenome[fidx]
+        if letter == '$':
+            break
+        genome.append(letter)
+        fidx = firstColIndex[letter] + lastColCount[fidx]
+
+    # reversed genome is the original genome
+    genome.reverse()
+    return ''.join(genome)
 
 genome = ibwt(genome)
-print(genome)
+#print(genome)
 
 ## output
 with open(outputFile, "w") as f:
